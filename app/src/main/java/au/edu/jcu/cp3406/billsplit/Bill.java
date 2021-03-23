fed970377c02c18a926839bc9dec9ce99e4a9b26
jackmckill <mr.mcgilljh@gmail.com>
2021-03-23 15:42:45 +1000

    Create Bill class with test.

---
+++ app/src/main/java/au/edu/jcu/cp3406/billsplit/Bill.java
@@ -0,0 +1,25 @@
+package au.edu.jcu.cp3406.billsplit;
+
+import java.text.DecimalFormat;
+
+public class Bill {
+    private float total;
+    private float numberOfPeople;
+    private float totalPerPerson;
+
+    public Bill(int total, int numberOfPeople) {
+        this.total = total;
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    public void calculateTotalPerPerson() {
+        totalPerPerson = total / numberOfPeople;
+    }
+
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("#.00");
+        String totalAsString = df.format(totalPerPerson);
+        return "$" + totalAsString;
+    }
+
+}
---
+++ app/src/test/java/au/edu/jcu/cp3406/billsplit/BillTests.java
@@ -0,0 +1,15 @@
+package au.edu.jcu.cp3406.billsplit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BillTests {
+
+    @Test
+    public void testStringandCalculation() {
+        Bill bill = new Bill(100, 3);
+        bill.calculateTotalPerPerson();
+        assertEquals("$33.33", bill.toString());
+    }
+}
