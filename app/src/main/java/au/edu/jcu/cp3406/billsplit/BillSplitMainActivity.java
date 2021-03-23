cebaee50402be7dc6d016010d1b8452d658f2f9e
jackmckill <mr.mcgilljh@gmail.com>
2021-03-23 13:44:40 +1000

    Initial commit

---
+++ app/src/main/java/au/edu/jcu/cp3406/billsplit/BillSplitMainActivity.java
@@ -0,0 +1,14 @@
+package au.edu.jcu.cp3406.billsplit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BillSplitMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
