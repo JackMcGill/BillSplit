6af114d288a1fe54c594a67ce36adf0307818b6d
jackmckill <mr.mcgilljh@gmail.com>
2021-03-23 15:57:05 +1000

    Basic setup of main activity

--- app/src/main/java/au/edu/jcu/cp3406/billsplit/BillSplitMainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/billsplit/BillSplitMainActivity.java
@@ -3,12 +3,24 @@ package au.edu.jcu.cp3406.billsplit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BillSplitMainActivity extends AppCompatActivity {
+
+    private Bill bill;
+    private EditText billAmount;
+    private EditText numberOfPeople;
+    private TextView totalPerPersonView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        billAmount = findViewById(R.id.billAmount);
+        numberOfPeople = findViewById(R.id.numberOfPeople)
+        totalPerPersonView = findViewById(R.id.totalPerPerson);
+
     }
 }
