4cf5b3977d7e5a8ae16b603b9ab2449399991143
jackmckill <mr.mcgilljh@gmail.com>
2021-03-23 15:48:30 +1000

    Fix spelling in test

--- app/src/test/java/au/edu/jcu/cp3406/billsplit/BillTests.java
+++ app/src/test/java/au/edu/jcu/cp3406/billsplit/BillTests.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class BillTests {
 
     @Test
-    public void testStringandCalculation() {
+    public void testStringAndCalculation() {
         Bill bill = new Bill(100, 3);
         bill.calculateTotalPerPerson();
         assertEquals("$33.33", bill.toString());
