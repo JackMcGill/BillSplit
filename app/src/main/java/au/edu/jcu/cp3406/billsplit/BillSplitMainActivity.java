69163f18d2f96cb5b7440c039ed5c39398ae4467
jackmckill <mr.mcgilljh@gmail.com>
2021-03-23 16:42:14 +1000

    Add bill splitting functionality

--- app/src/main/java/au/edu/jcu/cp3406/billsplit/BillSplitMainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/billsplit/BillSplitMainActivity.java
@@ -3,12 +3,12 @@ package au.edu.jcu.cp3406.billsplit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class BillSplitMainActivity extends AppCompatActivity {
 
-    private Bill bill;
     private EditText billAmount;
     private EditText numberOfPeople;
     private TextView totalPerPersonView;
@@ -19,8 +19,21 @@ public class BillSplitMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         billAmount = findViewById(R.id.billAmount);
-        numberOfPeople = findViewById(R.id.numberOfPeople)
+        numberOfPeople = findViewById(R.id.numberOfPeople);
         totalPerPersonView = findViewById(R.id.totalPerPerson);
 
+    }
+
+    public void calculateButtonClicked(View view) {
+
+        // Get user input from UI
+        int amount = Integer.parseInt(billAmount.getText().toString());
+        int people = Integer.parseInt(numberOfPeople.getText().toString());
+
+        // Create a Bill object
+        Bill bill = new Bill(amount, people);
+
+        bill.calculateTotalPerPerson();
+        totalPerPersonView.setText(bill.toString());
     }
 }
